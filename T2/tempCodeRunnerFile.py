60186d4c296305f17f8db79bd6bf3fa537cd0058
su_mu_zhe <2331810232@qq.com>
2023-06-23 15:29:00 +0800

    修整代码结构

--- T2/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
- circuit:
-        ending = ''
-        for i in range(len(x)):
-            print(x[i], end='')
-            if i == 0:
-                print()
-            elif i < len(x) - 1:
-                print('  ', end='')
-        print('\n
