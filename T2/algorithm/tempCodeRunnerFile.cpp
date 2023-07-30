418c8025149cd26fe1cdc1f6bd4bf2bea3677f49
Flash-LHR <2479082762@qq.com>
2023-07-30 14:14:23 +0800

    del useless file

--- T2/algorithm/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-t i = 0; i < 8; i++) printf(" %d(%d)", i, U[i].bit); printf("\n");
-    // printf("Y:"); for (int i = 0; i < 21; i++) printf(" %d(%d)", i, Y[i].bit); printf("\n");
-    // printf("T:"); for (int i = 0; i < 50; i++) printf(" %d(%d)", i, T[i].bit); printf("\n");
-    // printf("z:"); for (int i = 0; i < 18; i++) printf(" %d(%d)", i, z[i].bit); printf("\n");
-    // printf("S:"); for (int i
